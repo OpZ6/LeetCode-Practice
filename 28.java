f44f35bc41a264882958b6c4cee8d1ef30eb8c3e
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2023-12-17 20:38:50 -0800

    Create 28.java

---
+++ 28.java
@@ -0,0 +1,35 @@
+//28. Find the Index of the First Occurrence in a String
+//Easy
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i < haystack.length() - needle.length() + 1; i++){
+            for(int j = 0; j < needle.length(); j++){
+                if(needle.charAt(j) != haystack.charAt(i + j)){
+                    break;
+                }
+                if(j == needle.length() - 1){
+                    return i;                    
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
+//better way to get out of for loop
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) 
+        {
+            for (int j = 0; ; j++) 
+            {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+}
