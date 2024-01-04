d707874252e93a7bebb483d876701d65f395bc10
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2024-01-03 19:17:21 -0800

    Create 0344.java

---
+++ 0344.java
@@ -0,0 +1,15 @@
+#344. Reverse String
+#https://leetcode.com/problems/reverse-string/description/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length - 1;
+        while(start < end){
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
