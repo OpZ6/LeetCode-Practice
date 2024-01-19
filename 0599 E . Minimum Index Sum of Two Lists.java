6eb1a0db4cc284cee165d0e26cccb9d97cdb4dab
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2024-01-18 23:03:23 -0800

    Create 0599 E . Minimum Index Sum of Two Lists.java

---
+++ 0599 E . Minimum Index Sum of Two Lists.java
@@ -0,0 +1,62 @@
+// 599. Minimum Index Sum of Two Lists 
+// https://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+
+// Hashmap for sum and double loop
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap < Integer, List < String >> map = new HashMap < > ();
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    if (!map.containsKey(i + j))
+                        map.put(i + j, new ArrayList < String > ());
+                    map.get(i + j).add(list1[i]);
+                }
+            }
+        }
+        int min_index_sum = Integer.MAX_VALUE;
+        for (int key: map.keySet())
+            min_index_sum = Math.min(min_index_sum, key);
+        String[] res = new String[map.get(min_index_sum).size()];
+        return map.get(min_index_sum).toArray(res);
+    }
+}
+
+//loop by sum!!!
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        List < String > res = new ArrayList < > ();
+        for (int sum = 0; sum < list1.length + list2.length - 1; sum++) {
+            for (int i = 0; i <= sum; i++) {
+                if (i < list1.length && sum - i < list2.length && list1[i].equals(list2[sum - i]))
+                    res.add(list1[i]);
+            }
+            if (res.size() > 0)
+                break;
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
+
+//better hashmap
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List<String> res = new ArrayList<>();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
