597e7df46e467fb1f63d5e112ae795d25eba7892
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2024-05-22 14:29:27 +0800

    Update 0133 M . Clone Graph.java

--- 0133 M . Clone Graph.java
+++ 0133 M . Clone Graph.java
@@ -21,6 +21,7 @@ class Node {
 }
 */
 
+//DFS
 class Solution {
     public Node cloneGraph(Node node) {
         if (node == null)
@@ -41,3 +42,41 @@ class Solution {
         return clone;
     }
 }
+
+//BFS
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        
+        // 使用一个队列进行BFS
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        
+        // 创建一个映射来存储原始节点到克隆节点的映射
+        Map<Node, Node> clones = new HashMap<>();
+        
+        // 克隆第一个节点并存储到映射中
+        clones.put(node, new Node(node.val));
+        
+        // 进行BFS遍历
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            Node clone = clones.get(cur);
+            
+            // 遍历当前节点的所有邻居
+            for (Node neighbor : cur.neighbors) {
+                // 如果邻居还没有被克隆，则克隆并加入队列
+                if (!clones.containsKey(neighbor)) {
+                    clones.put(neighbor, new Node(neighbor.val));
+                    queue.offer(neighbor);
+                }
+                // 将邻居的克隆添加到当前节点的克隆的邻居列表中
+                clone.neighbors.add(clones.get(neighbor));
+            }
+        }
+        
+        // 返回克隆图的入口点
+        return clones.get(node);
+    }
+}
+
