63400a5eeab62c8941e967709654e2e72e56351a
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2024-05-23 19:04:06 +0800

    Create 0700 E . Search in a Binary Search Tree.java

---
+++ 0700 E . Search in a Binary Search Tree.java
@@ -0,0 +1,32 @@
+//700. Search in a Binary Search Tree
+//https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//BST LEFT < RIGHT
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
