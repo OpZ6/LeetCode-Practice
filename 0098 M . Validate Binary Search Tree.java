627c3524f525e36eda358c8bb27aabd71ec8d30e
Teo Luo <87977842+OpZ6@users.noreply.github.com>
2024-06-05 16:02:36 +0800

    Create 0098 M . Validate Binary Search Tree.java

---
+++ 0098 M . Validate Binary Search Tree.java
@@ -0,0 +1,121 @@
+//98. Validate Binary Search Tree
+//https://leetcode.com/problems/validate-binary-search-tree/editorial/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//set a range for node
+//Approach 1: Recursive Traversal with Valid Range
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
+
+//Approach 2: Iterative Traversal with Valid Range
+class Solution {
+    private Deque<TreeNode> stack = new LinkedList();
+    private Deque<Integer> upperLimits = new LinkedList();
+    private Deque<Integer> lowerLimits = new LinkedList();
+
+    public void update(TreeNode root, Integer low, Integer high) {
+        stack.add(root);
+        lowerLimits.add(low);
+        upperLimits.add(high);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        Integer low = null, high = null, val;
+        update(root, low, high);
+
+        while (!stack.isEmpty()) {
+            root = stack.poll();
+            low = lowerLimits.poll();
+            high = upperLimits.poll();
+
+            if (root == null) continue;
+            val = root.val;
+            if (low != null && val <= low) {
+                return false;
+            }
+            if (high != null && val >= high) {
+                return false;
+            }
+            update(root.right, val, high);
+            update(root.left, low, val);
+        }
+        return true;
+    }
+}
+
+//inorder
+class Solution {
+    // We use Integer instead of int as it supports a null value.
+    private Integer prev;
+
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        return inorder(root.right);
+    }
+}
+
+//WRONG! all node have to be satisfied！！
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null) {
+//             return true;
+//         }
+
+//         boolean leftTree = true, rightTree = true;
+//         if (root.left != null) {
+//             if (root.left.val >= root.val) {
+//                 return false;
+//             }
+//             leftTree = isValidBST(root.left);
+//         }
+//         if (root.right != null) {
+//             if (root.right.val <= root.val) {
+//                 return false;
+//             }
+//             rightTree = isValidBST(root.right);
+//         }
+//         return leftTree && rightTree;
+//     }
+// }
