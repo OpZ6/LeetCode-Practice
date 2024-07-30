fbeb7c9a8e42a3fd6cc2ad44f82ae43fdb28dcd7
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-07-30 15:00:22 +0800

    Update 0703 E . Kth Largest Element in a Stream.java

--- 0703 E . Kth Largest Element in a Stream.java
+++ 0703 E . Kth Largest Element in a Stream.java
@@ -1,40 +1,73 @@
 //703. Kth Largest Element in a Stream
 //https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
 
+
 class KthLargest {
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    int max = 0;
+    // insert a node into the BST
+    private Node insertNode(Node root, int num) {
+        if (root == null) {
+            return new Node(num, 1);
+        }
+        if (root.val < num) {
+            root.right = insertNode(root.right, num);
+        } else {
+            root.left = insertNode(root.left, num);
+        }
+        root.cnt++;
+        return root;
+    }
+
+    private int searchKth(Node root, int k) {
+        // m = the size of right subtree
+        int m = root.right != null ? root.right.cnt : 0;
+        // root is the m+1 largest node in the BST
+        if (k == m + 1) {
+            return root.val;
+        }
+        if (k <= m) {
+            // find kth largest in the right subtree
+            return searchKth(root.right, k);
+        } else {
+            // find (k-m-1)th largest in the left subtree
+            return searchKth(root.left, k - m - 1);
+        }
+    } 
+    
+    private Node root;
+    private int m_k;
 
     public KthLargest(int k, int[] nums) {
-        int len = nums.length;
-        max = k;
-        for (int i = 0; i < Math.min(len, k); i++) {
-        minHeap.add(nums[i]);
+        root = null;
+        for (int i = 0; i < nums.length; ++i) {
+            root = insertNode(root, nums[i]);
         }
-        if (nums.length > k) {
-            for (int i = k; i < len; i++) {
-                minHeap.add(nums[i]);
-                minHeap.poll();
-            }
-        }
-        // if (nums.length > k) {
-        //     for (int i = len; i < Math.min(len, k); i++) {
-        //         minHeap.add(nums[i]);
-        //         minHeap.poll();
-        //     }
-        // }
-        // System.out.println(minHeap.size());
-        // System.out.println(minHeap.peek());
+        m_k = k;
     }
-
+    
     public int add(int val) {
-        minHeap.add(val);
-        if (minHeap.size() > max) {
-            minHeap.poll();
-        }
-        return minHeap.peek();
+        root = insertNode(root, val);
+        return searchKth(root, m_k);
     }
 }
+
+class Node {    // the structure for the tree node
+    Node left;
+    Node right;
+    int val;
+    int cnt;    // the size of the subtree rooted at the node
+    public Node (int v, int c) {
+        left = null;
+        right = null;
+        val = v;
+        cnt = c;
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
 
 /**
  * Your KthLargest object will be instantiated and called as such:
