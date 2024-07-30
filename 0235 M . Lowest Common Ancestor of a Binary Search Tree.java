11830dac53873726474934327bc1626833a41441
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-07-30 15:38:13 +0800

    Create 0235 M . Lowest Common Ancestor of a Binary Search Tree.java

---
+++ 0235 M . Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,48 @@
+//235. Lowest Common Ancestor of a Binary Search Tree
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int min = Math.min(p.val, q.val);
+        int max = Math.max(p.val, q.val);
+
+        while (true) {
+            if (min <= root.val && root.val <= max) {
+                return root;
+            }
+
+            if (root.val < min) {
+                root = root.right;
+            }
+
+            if (root.val > max) {
+                root = root.left;
+            }            
+        }
+    }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        return root;
+    }
+}
