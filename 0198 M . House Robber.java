c5af505c9c02ea8ca219d3098cb040e56c22298e
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-08-05 16:58:29 +0800

    Update 0198 M . House Robber.java

--- 0198 M . House Robber.java
+++ 0198 M . House Robber.java
@@ -1,7 +1,7 @@
 //198. House Robber
 //https://leetcode.com/problems/house-robber/description/
 
-//dp
+//递归
 class Solution {
     private int[] memo;
 
@@ -27,5 +27,33 @@ class Solution {
 
         this.memo[i] = ans;
         return ans;
+    }
+}
+
+//迭代dp
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        
+        // dp数组存储到达每个位置能抢到的最大金额
+        int[] dp = new int[n];
+        
+        // 初始化前两个房子的情况
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        // 填充dp数组
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        
+        // dp[n-1]存储的是到达最后一个房子时能抢到的最大金额
+        return dp[n-1];
     }
 }
