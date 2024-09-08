0f49dfb71d3689f1d08ddcf5192abdce9edbee41
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-09-08 10:19:03 -0700

    Update 0039 M . Combination Sum.java

--- 0039 M . Combination Sum.java
+++ 0039 M . Combination Sum.java
@@ -26,3 +26,37 @@ class Solution {
         }
     }
 }
+
+//personal recode
+class Solution {
+    List<Integer> path = new ArrayList<Integer>();
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        rollBack(candidates, target, 0, 0);
+        return res;
+    }
+
+    public void rollBack(int[] nums, int target, int index, int sum) {
+        //找到结果
+        if (sum == target) {
+            res.add(new ArrayList<>(path));
+            // res.add(path); ERROR! Have to add a new object
+            return;
+        }
+        
+        //不可能存在结果直接切断路径尝试
+        if (sum > target) {
+            return;
+        }
+
+        //backtracking
+        for (int i = index; i < nums.length; i++) {//start from pos
+            path.add(nums[i]);
+            sum += nums[i];
+            rollBack(nums, target, i, sum);
+            sum -= nums[i];
+            path.remove(path.size() - 1);//pos
+        }
+    }
+}
