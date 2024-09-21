e2e65e15cd3685ee73c91c09b6f8592c005ba73d
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-09-21 00:59:17 -0700

    Create 1668 E . Maximum Repeating Substring.java

---
+++ 1668 E . Maximum Repeating Substring.java
@@ -0,0 +1,85 @@
+//1668. Maximum Repeating Substring
+//https://leetcode.com/problems/maximum-repeating-substring/description/
+
+//DP
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length();
+        int m = word.length();
+        int[] dp = new int[n + 1];
+        int maxRepeat = 0;
+
+        for (int i = m; i <= n; i++) {
+            if (sequence.substring(i - m, i).equals(word)) {
+                dp[i] = dp[i - m] + 1;
+                maxRepeat = Math.max(maxRepeat, dp[i]);
+            }
+        }
+
+        return maxRepeat;
+    }
+}
+
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+
+        int[] f = new int[n];
+        for (int i = m - 1; i < n; ++i) {
+            boolean valid = true;
+            for (int j = 0; j < m; ++j) {
+                if (sequence.charAt(i - m + j + 1) != word.charAt(j)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
+            }
+        }
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+}
+
+//KMP
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+
+        int[] fail = new int[m];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < m; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && word.charAt(j + 1) != word.charAt(i)) {
+                j = fail[j];
+            }
+            if (word.charAt(j + 1) == word.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+
+        int[] f = new int[n];
+        int j = -1;
+        for (int i = 0; i < n; ++i) {
+            while (j != -1 && word.charAt(j + 1) != sequence.charAt(i)) {
+                j = fail[j];
+            }
+            if (word.charAt(j + 1) == sequence.charAt(i)) {
+                ++j;
+                if (j == m - 1) {
+                    f[i] = (i >= m ? f[i - m] : 0) + 1;
+                    j = fail[j];
+                }
+            }
+        }
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+}
