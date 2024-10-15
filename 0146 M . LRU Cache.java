f6c17097163d8968d819e5ad21dde55055fef91d
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-10-14 18:55:15 -0700

    Update 0146 M . LRU Cache.java

--- 0146 M . LRU Cache.java
+++ 0146 M . LRU Cache.java
@@ -82,3 +82,59 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+    
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
+    }
+}
+
+class LRUCache {
+    private final int capacity;
+    private final Map<Integer, Integer> cache = new LinkedHashMap<>(); // 自带双向链表
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        // 删除 key，并利用返回值判断 key 是否在 cache 中
+        Integer value = cache.remove(key);
+        if (value != null) { // key 在 cache 中
+            cache.put(key, value); // 把 key 移到链表末尾
+            return value;
+        }
+        // key 不在 cache 中
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        // 删除 key，并利用返回值判断 key 是否在 cache 中
+        if (cache.remove(key) != null) { // key 在 cache 中
+            cache.put(key, value); // 把 key 移到链表末尾
+            return;
+        }
+        // key 不在 cache 中，那么就把 key 插入 cache，插入前判断 cache 是否满了
+        if (cache.size() == capacity) { // cache 满了
+            Integer oldestKey = cache.keySet().iterator().next();
+            cache.remove(oldestKey); // 移除最久未使用 key
+        }
+        cache.put(key, value);
+    }
+}
