c14c7aa347362cb90c1edea2948f7815dc68c804
TeoLuo <87977842+OpZ6@users.noreply.github.com>
2024-11-24 23:13:27 -0800

    Update 0322 M . Coin Change.java

--- 0322 M . Coin Change.java
+++ 0322 M . Coin Change.java
@@ -18,3 +18,28 @@ class Solution {
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        // for (int i = 0; i <= amount; i++) {
+        //     dp[i] = Integer.MAX_VALUE;
+        // }
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        // if (dp[amount] == Integer.MAX_VALUE) {
+        //     return -1;
+        // }
+        // return dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
